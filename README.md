89d1df6211ad0f0c0641eebff475dbf8a9246599
Marco <bigdatajang@gmail.com>
2023-04-16 20:01:29 +0900

    feat: 데이터베이스를 생성하고 모델을 서버와 연결

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodebird
+
+## 명령어
+
+- 데이터베이스 생성
+
+```bash
+npx sequelize db:create
+```
+
+- 서버 실행
+
+```bash
+npm start
+```
--- models/index.js
+++ models/index.js
@@ -1,43 +1,53 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
+
+const User = require("./user");
+const Post = require("./post");
+const HashTag = require("./hashtag");
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	config
+);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// db.User = User;
+// db.Post = Post;
+// db.Hashtag = Hashtag;
+
+// User.initiate(sequ)
+
+fs.readdirSync(__dirname) // models 폴더 안에 있는 파일들을 읽어옴
+	.filter(file => {
+		// 숨감파일, index.js, .js 확장자가 아닌 파일 필터링
+		return (
+			file.indexOf(".") !== 0 &&
+			file !== basename &&
+			file.slice(-3) === ".js"
+		);
+	})
+	.forEach(file => {
+		// 해당 파일의 모델을 불러와서 init
+		const model = require(path.join(__dirname, file));
+		db[model.name] = model.init(sequelize);
+	});
+
+Object.keys(db).forEach(modelName => {
+	// associate 호출
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
+});
 
 module.exports = db;
